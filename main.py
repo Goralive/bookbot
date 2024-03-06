6fad32ce92ad0467b40ffd4a6e5a309202099b2e
Igor Nosovskyi <igor.nosovskyi@flixbus.com>
2024-03-06 17:28:57 +0100

    count_words funcionality

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ def main():
     book_path = "books/frankenstein.txt"
     text = text_content_as_string(book_path)
     print(text)
+    print(f"Size of text is: {count_words(text)}")
 
 
 def text_content_as_string(path):
     with open(path, encoding="UTF-8") as file:
         return file.read()
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 
 main()
