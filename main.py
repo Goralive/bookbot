e983f2373c1725219d2c2e602d3976e43c64deae
Igor Nosovskyi <igor.nosovskyi@flixbus.com>
2024-03-07 17:46:55 +0100

    add count letters task

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = text_content_as_string(book_path)
     print(text)
     print(f"Size of text is: {count_words(text)}")
+    letters = count_letters(text)
+    print(letters)
 
 
 def text_content_as_string(path):
@@ -13,6 +15,17 @@ def text_content_as_string(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def count_letters(text):
+    chars = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
 
 
 main()
